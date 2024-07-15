baf8bea6c2974ef6f480604a9184cc80bde85e86
Hypocrite1023 <rex901023wot@gmail.com>
2024-07-15 19:21:43 +0800

    Initial Commit

---
+++ AnimeTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnimeTracker
+//
+//  Created by 邱翊均 on 2024/7/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
