863da64e157d50eedac6bee19877684719c8945a
Hypocrite1023 <rex901023wot@gmail.com>
2024-07-25 14:52:56 +0800

    完成anime detail page relation view的部分

---
+++ AnimeTracker/View/RelationPreview/RelationPreview.swift
@@ -0,0 +1,20 @@
+//
+//  RelationPreview.swift
+//  AnimeTracker
+//
+//  Created by 邱翊均 on 2024/7/24.
+//
+
+import UIKit
+
+class RelationPreview: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ AnimeTracker/View/RelationView/RelationView.swift
@@ -0,0 +1,20 @@
+//
+//  RelationView.swift
+//  AnimeTracker
+//
+//  Created by 邱翊均 on 2024/7/24.
+//
+
+import UIKit
+
+class RelationView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
